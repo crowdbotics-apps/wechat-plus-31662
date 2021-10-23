bef5fee8fcc597a41edae5ddc5e1f24c3adb27bf
Crowdbotics <team@crowdbotics.com>
2021-10-23 08:23:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wechat_plus_31662",
+  url: "https://wechat_plus_31662.botics.co",
+  api: "https://wechat_plus_31662.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
